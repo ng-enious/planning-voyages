9056c223b1cac76f423ccbc7f679acc9e4ce87da
debbabinjouma <debbabinjouma@gmail.com>
2017-04-04 08:52:18 -0400

    map test

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,11 +1,44 @@
 <style>
-	#map_canvas{
-		width: 250px;
-		height: 150 px;
-		
+	#map {
+		width:250px;
+		height :350px;
 	}
 </style>
-<link href="{{ url('css/bootstrap.css') }}" rel="stylesheet" type="text/css" media="all">
- <script type="text/javascript" src='https://maps.google.com/maps/api/js?sensor=false&libraries=places'></script>
-<script src="{{ URL::asset('js/locationpicker.jquery.js')}}"></script>
-<style type="text/css"> </script>
+   <link rel="stylesheet" href="/maps/documentation/javascript/demos/demos.css">
+<script type="text/javascript"
+         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDqSTkzPn8PpJBY3Pclu-TTjmGDLzqKMD4&libraries=places&callback=initMap">
+    </script>
+<div class="container">
+	<div class="col-sm-4">
+	   	<h1> addd lieu</h1>
+	
+		<div class="form-group">
+			<label for="">titre</label>
+			<input type="text" class="form-control input-sm" name="title">
+		</div>
+				<div class="form-group">
+			<label for="">map</label>
+					
+			<input type="text" id="searchamp">
+					<div id="map" style="position:fixed;top:0:left:0;height:50%;width:50%;"></div>
+					</div>
+			<div class="form-group">
+			<label for="">lng</label>
+			<input type="text" class="form-control input-sm" name="lng">
+		</div>
+			<div class="form-group">
+			<label for="">lat</label>
+			<input type="text" class="form-control input-sm" name="lat">
+		</div>
+
+		</div>
+
+	</div>
+  <script>
+		 var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 27.27, lng: 150.644},
+          
+          zoom: 8
+			 
+		 });</script>
+
