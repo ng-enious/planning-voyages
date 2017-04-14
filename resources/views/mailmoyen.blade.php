dfa757ef33fad8ceb696fee15229be83c82486c2
debbabinjouma <debbabinjouma@gmail.com>
2017-04-14 06:22:54 -0400

    email  ajouter pagination

--- resources/views/mailmoyen.blade.php
+++ resources/views/mailmoyen.blade.php
@@ -5,6 +5,6 @@
     <title>Directions service</title>
   </head>
   <body>
-    <h1>desole votre nouveau moyen ajouter ne sera pas ajouté</h1>
+    <h1>desole votre nouveau moyen ajouter ne sera pas ajouté </h1>
   </body>
 </html>
